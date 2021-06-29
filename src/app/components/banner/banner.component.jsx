cfae838cf832cd58acc8fcfbe91c8255af03b86c
DESKTOP-5U330HI\fvtie <fvtien@gmail.com>
2021-06-30 03:34:29 +0700

    fixed banner

--- src/app/components/banner/banner.component.jsx
+++ src/app/components/banner/banner.component.jsx
@@ -1,32 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
-
-import "./banner.component.scss";
+import { Carousel } from "antd";
 import { mockBanner } from "./mock/mock_banner";
+import "./banner.component.scss";
 
 const Banner = () => {
   return (
-    <div className="banner">
-      <OwlCarousel
-        className="owl-theme"
-        loop
-        nav={false}
-        mouseDrag
-        autoplay
-        items={1}
-      >
-        {mockBanner.map(item => (
-          <div key={item.banner_id} className="banner__item">
-            <Link to={item.link}>
-              <img src={item.image} alt={item.title} />
-            </Link>
-          </div>
-        ))}
-      </OwlCarousel>
-    </div>
+    <Carousel autoplay draggable dotPosition="bottom">
+      {mockBanner.map(banner => (
+        <div key={banner.banner_id} className="banner__item">
+          <img src={banner.image} alt={banner.name} />
+        </div>
+      ))}
+    </Carousel>
   );
 };
 
--- src/app/components/banner/mock/mock_banner.js
+++ src/app/components/banner/mock/mock_banner.js
@@ -1,27 +1,24 @@
-import banner_1 from "./images/banner-1.jpg";
-import banner_2 from "./images/banner-2.jpg";
-import banner_3 from "./images/banner-3.jpg";
+import bannerImage_1 from "./images/banner-1.jpg";
+import bannerImage_2 from "./images/banner-2.jpg";
+import bannerImage_3 from "./images/banner-3.jpg";
 
 export const mockBanner = [
   {
     banner_id: 1,
     title: "banner 1",
-    image: banner_1,
-    link: "",
+    image: bannerImage_1,
     description: "",
   },
   {
     banner_id: 2,
     title: "banner 2",
-    image: banner_2,
-    link: "",
+    image: bannerImage_2,
     description: "",
   },
   {
     banner_id: 3,
     title: "banner 3",
-    image: banner_3,
-    link: "",
+    image: bannerImage_3,
     description: "",
   },
 ];
